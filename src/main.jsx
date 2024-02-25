1c0e283111b9b0a09b8495c69927c448287aecd9
stupidJoon <junsang.yu3@gmail.com>
2024-02-25 10:57:09 +0900

    ga4?

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, useLocation, BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Index from './routes/index';
 import Channel from './routes/channel';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Index />,
-  },
-  {
-    path: "/:channelID",
-    element: <Channel />,
-  },
-]);
+// if (import.meta.env.GA4_ID) {
+//   ReactGA.initialize(import.meta.env.GA4_ID);
+// }
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Index />,
+//   },
+//   {
+//     path: "/:channelID",
+//     element: <Channel />,
+//   },
+// ]);
+
+function App() {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (import.meta.env.GA4_ID) {
+      ReactGA.initialize(import.meta.env.GA4_ID);
+      setInitialized(true);
+    }
+  }, []);
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send("pageview");
+    }
+  }, [initialized, location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/:channelID" element={<Channel />} />
+    </Routes>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig, loadEnv  } from 'vite';
+// import { defineConfig, loadEnv  } from 'vite';
+import { defineConfig  } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePluginRadar } from 'vite-plugin-radar';
+// import { VitePluginRadar } from 'vite-plugin-radar';
 
 // process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
@@ -18,29 +19,29 @@ import { VitePluginRadar } from 'vite-plugin-radar';
 // }
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins,
-// });
+export default defineConfig({
+  plugins: [react()],
+});
 
 // export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
 
 // });
 
-export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+// export default ({ mode }) => {
+//   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-  const plugins = [react()];
+//   const plugins = [react()];
 
-  if (process.env.GA4_ID) {
-    plugins.push(
-      VitePluginRadar({
-        // Google Analytics tag injection
-        analytics: {
-          id: process.env.GA4_ID,
-        },
-      }),
-    );
-  }
+//   if (process.env.GA4_ID) {
+//     plugins.push(
+//       VitePluginRadar({
+//         // Google Analytics tag injection
+//         analytics: {
+//           id: process.env.GA4_ID,
+//         },
+//       }),
+//     );
+//   }
 
-  return defineConfig({ plugins })
-}
+//   return defineConfig({ plugins })
+// }
