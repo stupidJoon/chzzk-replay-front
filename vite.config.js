321b0d4283997732367f69d65d806facb8e5cdf9
stupidJoon <junsang.yu3@gmail.com>
2024-02-25 09:38:49 +0900

    add ga4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginRadar } from 'vite-plugin-radar';
+
+const plugins = [react()];
+
+if (import.meta.env.GA4_ID) {
+  plugins.push(
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: import.meta.env.GA4_ID,
+      },
+    }),
+  );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins,
+});
