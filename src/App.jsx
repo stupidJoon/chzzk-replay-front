357628313bc2d97d5023e5b4b3b2518c57f03d2b
stupidJoon <junsang.yu3@gmail.com>
2024-02-25 12:32:44 +0900

    modify

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import { useLocation, Routes, Route } from "react-router-dom";
+
+import ReactGA from "react-ga4";
+import Index from './routes/index';
+import Channel from './routes/channel';
+
+function App() {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (import.meta.env.VITE_GA4_ID) {
+      console.log('GA initialized!')
+      ReactGA.initialize(import.meta.env.VITE_GA4_ID);
+      setInitialized(true);
+    }
+  }, []);
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send("pageview");
+    }
+  }, [initialized, location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/:channelID" element={<Channel />} />
+    </Routes>
+  )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useLocation, BrowserRouter, Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
-import Index from './routes/index';
-import Channel from './routes/channel';
-
-
-function App() {
-  const location = useLocation();
-  const [initialized, setInitialized] = useState(false);
-
-  useEffect(() => {
-    if (import.meta.env.VITE_GA4_ID) {
-      console.log('GA initialized!')
-      ReactGA.initialize(import.meta.env.VITE_GA4_ID);
-      setInitialized(true);
-    }
-  }, []);
-  useEffect(() => {
-    console.log(initialized, location);
-    if (initialized) {
-      console.log('Page Changed!', location.pathname);
-      ReactGA.set({ page: location.pathname });
-      ReactGA.send("pageview");
-    }
-  }, [initialized, location]);
-
-  return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/:channelID" element={<Channel />} />
-    </Routes>
-  )
-}
-
+import { BrowserRouter } from "react-router-dom";
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -41,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
