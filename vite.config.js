3e6c63371d316adf9d58403d8037f3c8ba64d0d4
stupidJoon <junsang.yu3@gmail.com>
2024-02-25 09:46:20 +0900

    debug ga4

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,46 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv  } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePluginRadar } from 'vite-plugin-radar';
 
-const plugins = [react()];
+// process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
-if (import.meta.env.GA4_ID) {
-  plugins.push(
-    VitePluginRadar({
-      // Google Analytics tag injection
-      analytics: {
-        id: import.meta.env.GA4_ID,
-      },
-    }),
-  );
-}
+// const plugins = [react()];
+
+// if (process.env.GA4_ID) {
+//   plugins.push(
+//     VitePluginRadar({
+//       // Google Analytics tag injection
+//       analytics: {
+//         id: process.env.GA4_ID,
+//       },
+//     }),
+//   );
+// }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins,
-});
+// export default defineConfig({
+//   plugins,
+// });
+
+// export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+
+// });
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  const plugins = [react()];
+
+  if (process.env.GA4_ID) {
+    plugins.push(
+      VitePluginRadar({
+        // Google Analytics tag injection
+        analytics: {
+          id: process.env.GA4_ID,
+        },
+      }),
+    );
+  }
+
+  return defineConfig({ plugins })
+}
